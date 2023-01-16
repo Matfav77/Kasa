51a645311f66121fbf8cbf05fe22eb9225e883e7
Mathieu Favre <matfav@hotmail.fr>
2023-01-16 14:29:32 +0100

    Worked on Home page's responsiveness, started creation of Slideshow component

---
+++ src/components/UI/SlideShow.js
@@ -0,0 +1,13 @@
+
+import styles from './SlideShow.module.css'
+
+
+
+const SlideShow = props => {
+
+    return <div className={styles.mainframe}>
+
+    </div>
+}
+
+export default SlideShow;
--- src/pages/Logement.js
+++ src/pages/Logement.js
@@ -1,14 +1,17 @@
 import { useParams } from "react-router-dom";
-import allRentals from '../data/logements.json'
+import allRentals from '../data/logements.json';
+import styles from './Logements.module.css';
+import SlideShow from '../components/UI/SlideShow'
 
 const Logement = props => {
 
     const { id } = useParams();
     const foundAccomodation = allRentals.filter(e => e.id === id);
 
-
-
-    return <h1>Logement {id} </h1>
+    return <>
+        <h1>Logement {id} </h1>
+        <SlideShow />
+    </>
 }
 
 export default Logement
